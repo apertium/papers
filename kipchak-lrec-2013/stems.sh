a7aa0167f97c03571f916465d7e267ab1b5da07f
Jonathan North Washington <jonathan.n.washington@gmail.com>
2013-10-24 23:46:06 +0000

    some updates and fixes and expanding
    git-svn-id: https://svn.code.sf.net/p/apertium/svn/branches/papers/@48180 72bbbca6-d526-0410-a7d9-f06f51895060

--- kipchak-lrec-2013/stems.sh
+++ kipchak-lrec-2013/stems.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 pushd $APERTIUMPATH/languages/apertium-kum/dev/
+	hfst-lexc --format foma ../apertium-kum.kum.lexc -o /dev/null
 	./countstems.sh
 popd
 
 pushd $APERTIUMPATH/languages/apertium-kaz/dev/
+	hfst-lexc --format foma ../apertium-kaz.kaz.lexc -o /dev/null
 	./countstems.sh
 popd
 
-pushd $APERTIUMPATH/languages/apertium-kaz/dev/
+pushd $APERTIUMPATH/languages/apertium-tat/dev/
+	hfst-lexc --format foma ../apertium-tat.tat.lexc -o /dev/null
 	./countstems.sh
 popd
 
